ff5e7af9ec0eea324fbe57c875d86c721543ace7
nbhansali06@gmail.com <nbhansali06@gmail.com>
2025-01-25 15:10:03 +0530

    feat: text area with button

--- application/frontend/smollm-frontend/app/page.tsx
+++ application/frontend/smollm-frontend/app/page.tsx
@@ -1,10 +1,13 @@
 import { ButtonDemo } from "@/components/Button";
+import { TextareaWithButton } from "@/components/TextAreaWithButton";
 
 export default function Home() {
   return (
-    <main>
-      <div>Hello world from smollm-frontend!</div>
-      <ButtonDemo />
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-2xl font-bold mb-4">SmolLM Frontend</h1>
+      <div className="w-1/2">
+        <TextareaWithButton />
+      </div>
     </main>
   );
 }
